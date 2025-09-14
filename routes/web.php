06dff5338a5198db9d61b78da77124aefa16cc26
Shareiar <islamshareiar@gmail.com>
2025-09-15 00:56:30 +0600

    add seperate route files  for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+// ========================================
+// ADMIN ROUTES
+// ========================================
+
+Route::prefix('admin')->name('admin.')->group( function () {
+    require __DIR__.'/Admin/auth.php';
+    
+    // ========================================
+    // AUTHENTICATED ADMIN ROUTES
+    // ========================================
+    Route::middleware(['admin'])->group(function () {
+        require __DIR__.'/Admin/dashboard.php';
+    });
 });
