5176bb07839f2414c7499b5a622dad2458b44e2b
Shareiar <islamshareiar@gmail.com>
2025-09-15 00:56:55 +0600

    all kind of dashboard related route located  here

---
+++ routes/Admin/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Admin\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(HomeController::class)->group( function () {
+    // ========================================
+    // ADMIN DASHBOARD
+    // ========================================
+    Route::get('dashboard', 'index')->name('home');
+    Route::get('profile', 'create')->name('profile');
+
+    // Admin Profile Management
+    Route::post('image/update/{id}', 'imgupdate')->name('image.update');
+    Route::put('password/update/{id}', 'passupdate')->name('password.update');
+    Route::put('profile/update/{id}', 'update')->name('profile.update');
+    
+    // Cache Management
+    Route::get('cache/clear', 'cache')->name('cache.clear');
+});
+// Main Controller Resource
+Route::resource('homecontroller', HomeController::class);
