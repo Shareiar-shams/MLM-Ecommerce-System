3edb318d2451b3fd730c29dc8546fd75a0861191
Shareiar <islamshareiar@gmail.com>
2025-09-15 03:23:08 +0600

    Change validation rules

--- app/Http/Requests/Admin/ImageUploadRequest.php
+++ app/Http/Requests/Admin/ImageUploadRequest.php
@@ -22,7 +22,7 @@ class ImageUploadRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
         ];
     }
 }
