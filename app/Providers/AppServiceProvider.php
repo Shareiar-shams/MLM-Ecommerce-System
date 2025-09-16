3e06452007e88c58acab753249b894c95d7becf4
Shareiar <islamshareiar@gmail.com>
2025-09-17 00:59:45 +0600

    passs notification to all the website based on user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            if (auth('admin')->check()) {
+                $notifications = auth('admin')->user()->unreadNotifications()->take(5)->get();
+                $view->with('notifications', $notifications);
+            }
+        });
     }
 }
