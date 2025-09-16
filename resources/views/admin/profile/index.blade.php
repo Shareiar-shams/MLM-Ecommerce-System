1f711f3e9243f17dca1d3ff67682cc3b1498653d
scottShams <scott123.staff.india@gmail.com>
2025-09-16 20:27:19 +0600

    Change Image path

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -36,7 +36,7 @@
 				            	@if(Auth::guard('admin')->user()->image != 'noimage.jpg')
 					              	<label for="file" style="cursor: pointer; display: inline;"><img src="{{ Auth::guard('admin')->user()->thumbnail }}" class="profile-user-img img-responsive img-circle" alt="User profile picture" id="output"></label>
 					            @else
-					              	<label for="file" style="cursor: pointer; display: inline;"><img src="{{asset('admin/dist/img/avatar4.png')}}" class="profile-user-img img-responsive img-circle" alt="User profile picture" id="output"></label>
+					              	<label for="file" style="cursor: pointer; display: inline;"><img src="{{asset('admin/assets/img/avatar4.png')}}" class="profile-user-img img-responsive img-circle" alt="User profile picture" id="output"></label>
 					            @endif
 
 								<input type="submit" class="btn btn-primary btn-block" style="font-weight: bold;" value="Change Profile Picture">
