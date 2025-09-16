505444e545bdce26a3fdb8999cd8792a81ddea5a
scottShams <scott123.staff.india@gmail.com>
2025-09-16 19:58:52 +0600

    Add all kind of error include with server error and maintence mood and read data from ErrorConfiguration files and pass it to render it to view

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,8 +2,10 @@
 
 use App\Http\Middleware\AdminAuth;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use App\Exceptions\ErrorConfiguration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,23 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $e, $request) {
+            $statusCode = $e instanceof HttpExceptionInterface
+                ? $e->getStatusCode()
+                : ($e->getCode() && $e->getCode() >= 100 && $e->getCode() < 600
+                    ? $e->getCode()
+                    : 500);
+            
+
+            $errorData = ErrorConfiguration::getErrorData($statusCode);
+            
+            $view = $errorData['view'] ?? ErrorConfiguration::getErrorView();
+            return response()->view($view, [
+                'statusCode' => $statusCode,
+                'title' => $errorData['title'],
+                'message' => $errorData['message'],
+                'image' => $errorData['image'],
+                'exception' => $e,
+            ], $statusCode);
+        });
     })->create();
