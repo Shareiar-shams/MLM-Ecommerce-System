c12187d902a1ca04633dbb1034225180c4a3d6fd
scottShams <scott123.staff.india@gmail.com>
2025-09-17 20:01:53 +0600

    add: Read actual error message and file name and errorline and pass it to view file for infor develop where error happend. and also generate at log file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use App\Exceptions\ErrorConfiguration;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,7 +27,11 @@ return Application::configure(basePath: dirname(__DIR__))
                 : ($e->getCode() && $e->getCode() >= 100 && $e->getCode() < 600
                     ? $e->getCode()
                     : 500);
-            
+            $errorMessage = $e->getMessage();
+            $errorFile    = $e->getFile();
+            $errorLine    = $e->getLine();
+
+            Log::error("Exception: {$errorMessage} in {$errorFile} on line {$errorLine}");
 
             $errorData = ErrorConfiguration::getErrorData($statusCode);
             
@@ -34,7 +39,7 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->view($view, [
                 'statusCode' => $statusCode,
                 'title' => $errorData['title'],
-                'message' => $errorData['message'],
+                'message' => "Exception: {$errorMessage} in {$errorFile} on line {$errorLine}" ?? $errorData['message'],
                 'image' => $errorData['image'],
                 'exception' => $e,
             ], $statusCode);
