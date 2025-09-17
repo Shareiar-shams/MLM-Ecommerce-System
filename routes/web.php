f0e994f9bad97f50289f6bbc977883cead82db28
Shareiar <islamshareiar@gmail.com>
2025-09-18 03:59:55 +0600

    update: Add more routes

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,9 @@ Route::prefix('admin')->name('admin.')->group( function () {
     // ========================================
     Route::middleware(['admin', 'localization'])->group(function () {
         require __DIR__.'/Admin/dashboard.php';
+        // Product Categories
+        require __DIR__.'/Admin/categories.php';
+        // localization
+        require __DIR__.'/Admin/localization.php';
     });
 });
