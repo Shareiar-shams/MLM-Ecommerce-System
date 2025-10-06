1aeb3521c4ff126dc9eb78db9faed9eb00da9e8b
scottShams <scott123.staff.india@gmail.com>
2025-10-06 22:36:32 +0600

    using this service handle all categories controller method functionallity

--- app/Services/Admin/Categories/CategoriesService.php
+++ app/Services/Admin/Categories/CategoriesService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Admin\Categories;
 
+use App\Models\Categories\Categories;
 use App\Services\Admin\ImageService;
 
 class CategoriesService
@@ -11,6 +12,15 @@ class CategoriesService
     public function __construct(ImageService $imageService)
     {
         $this->imageService = $imageService;
+    }
+
+    public function createCategory(array $data)
+    {
+        if (isset($data['image'])) {
+            $data['image'] = $this->imageService->storeSingleImage($data['image'], 'categories');
+        }
+
+        return Categories::create($data);
     }
 
 }
