ea7132c03c15060bcd29fbab6a56d0294a5d5332
scottShams <scott123.staff.india@gmail.com>
2025-10-06 22:35:48 +0600

    store single image without any differnt size

--- app/Services/Admin/ImageService.php
+++ app/Services/Admin/ImageService.php
@@ -14,6 +14,38 @@ class ImageService
         $this->imageManager = new ImageManager(new Driver());
     }
 
+    /**
+     * Store a single image without creating variants
+     *
+     * @param \Illuminate\Http\UploadedFile $file
+     * @param string $folder
+     * @param string|null $filename
+     * @param int|null $width
+     * @param int|null $height
+     * @return string
+     */
+    public function storeSingleImage($file, $folder, $filename = null, $width = null, $height = null)
+    {
+        $filename = $filename ?? uniqid() . '.' . $file->getClientOriginalExtension();
+        $path = storage_path('app/public/' . $folder);
+
+        // Create directory if it doesn't exist
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        // If width and height are provided, resize the image
+        if ($width && $height) {
+            $image = $this->imageManager->read($file)->cover($width, $height);
+            $image->save($path . '/' . $filename);
+        } else {
+            // Store original image without resizing
+            $file->storeAs($folder, $filename, 'public');
+        }
+
+        return $filename;
+    }
+
     public function uploadAndResize($file, $folder, $filename = null)
     {
         $filename = $filename ?? uniqid().'.'.$file->getClientOriginalExtension();
@@ -49,5 +81,17 @@ class ImageService
     public function getImageUrl($folder, $filename, $type = 'original')
     {
         return asset("storage/{$folder}/{$type}/{$filename}");
+    }
+
+    /**
+     * Get URL for a single stored image
+     *
+     * @param string $folder
+     * @param string $filename
+     * @return string
+     */
+    public function getSingleImageUrl($folder, $filename)
+    {
+        return asset("storage/{$folder}/{$filename}");
     }
 }
