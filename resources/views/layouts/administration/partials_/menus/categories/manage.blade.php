f347ca9a8131237aaea9452b2f13302465e52953
scottShams <scott123.staff.india@gmail.com>
2025-10-06 22:17:25 +0600

    add active nav class and fixed the style tag add issue

--- resources/views/layouts/administration/partials_/menus/categories/manage.blade.php
+++ resources/views/layouts/administration/partials_/menus/categories/manage.blade.php
@@ -1,21 +1,24 @@
 <!-- File: resources/views/admin/layouts/partials_/sidebar-nav/categories/manage.blade.php -->
-<div class="nav-item">
-    <x-ad-nav-link class="nav-link">
+@php
+    $isActive = Route::is('admin.product.categories') || Route::is('admin.product.category.create');
+@endphp
+<div class="nav-item {{ $isActive ? 'menu-open' : '' }}">
+    <x-ad-nav-link class="nav-link {{ $isActive ? 'active' : '' }}">
         <i class="nav-icon fas fa-list-alt"></i>
         <p class="pl-2">
             Manage Categories
             <i class="fas fa-angle-left right"></i>
         </p>
     </x-ad-nav-link>
-    <ul class="nav nav-treeview">
+    <ul class="nav nav-treeview" style="display: {{ $isActive ? 'block' : 'none' }}">
         <li class="nav-item">
-            <x-ad-nav-link href="" class="nav-link">
+            <x-ad-nav-link href="{{ route('admin.product.categories') }}" class="nav-link {{ Route::is('admin.product.categories') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Categories List</p>
             </x-ad-nav-link>
         </li>
         <li class="nav-item">
-            <x-ad-nav-link href="" class="nav-link">
+            <x-ad-nav-link href="{{ route('admin.categories.create') }}" class="nav-link {{ Route::is('admin.product.category.create') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Create Category</p>
             </x-ad-nav-link>
