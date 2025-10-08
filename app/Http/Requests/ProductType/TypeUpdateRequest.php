6a095fc97eb1c1d3dc99792add9f488c0cbdd920
scottShams <scott123.staff.india@gmail.com>
2025-10-08 17:15:59 +0600

    add data inside validation method.

--- app/Http/Requests/ProductType/TypeUpdateRequest.php
+++ app/Http/Requests/ProductType/TypeUpdateRequest.php
@@ -11,7 +11,7 @@ class TypeUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class TypeUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|unique:product_types,name,'.$this->route('id'),
+            'slug' => 'required|unique:product_types,slug,'.$this->route('id'),
         ];
     }
 }
