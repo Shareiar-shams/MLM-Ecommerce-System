9533dcc06b0265140eaf16ff2636dd5f66ce5f10
scottShams <scott123.staff.india@gmail.com>
2025-10-08 18:14:03 +0600

    define products table schema

---
+++ database/migrations/2025_10_08_114438_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->unique();
+            $table->string('affiliate_link')->nullable();
+            $table->string('featured_image')->nullable();
+            $table->json('gallery_images')->nullable();
+            $table->text('short_description')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('product_type');
+            $table->json('tags')->nullable();
+            $table->json('specifications')->nullable();
+            $table->unsignedInteger('stock')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('special_price', 10, 2)->nullable();
+            $table->string('video_link')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->decimal('customize_charge', 10, 2)->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
