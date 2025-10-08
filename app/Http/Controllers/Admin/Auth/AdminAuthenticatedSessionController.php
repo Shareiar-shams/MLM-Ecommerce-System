bab034daf8ee3971b10b1b6b92b8db78e235e5a8
scottShams <scott123.staff.india@gmail.com>
2025-10-08 16:10:52 +0600

    remove undefined import

--- app/Http/Controllers/Admin/Auth/AdminAuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AdminAuthenticatedSessionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AdminLoginRequest;
-use App\Http\Responses\LoginResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
