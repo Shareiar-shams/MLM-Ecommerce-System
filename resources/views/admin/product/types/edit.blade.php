69ad6e6be0f02c49c1d936004b3a30465939242f
scottShams <scott123.staff.india@gmail.com>
2025-10-08 16:35:01 +0600

    add create and edit file to create or update any types

---
+++ resources/views/admin/product/types/edit.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.administration.app')
+@section('admin_title_content')
+    AHVision | Product Types
+@endsection
+@section('admin_content_header')
+    <div class="col-sm-6">
+        <h1 class="m-0">{{___('Product Types Edit')}}</h1>
+    </div><!-- /.col -->
+    <!-- breadcrumb -->
+    <x-ad-breadcrumb :items="[
+        ['label' => 'Dashboard', 'url' => route('admin.home')],
+        ['label' => 'Types', 'url' => route('admin.product.type.index')]
+    ]" />
+@endsection
+@section('admin_vendor_css')
+	<!-- summernote -->
+  	<link rel="stylesheet" href="{{asset('admin/assets/plugins/summernote/summernote-bs4.min.css')}}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('admin/assets/plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+@endsection
+@section('admin_page_css')
+@endsection
+
+@section('admin_main_content')
+	<!-- Display Validation Error -->
+	@include('admin.validationError.error')
+    <!-- container-fluid -->
+	<div class="container-fluid">
+        <div class="card">
+			<div class="card-header">
+				<a class="btn btn-primary btn-sm float-right" href="{{ route('admin.product.type.index') }}"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Back</a>
+			</div>
+		</div>
+    	<form action="{{route('admin.product.type.update', ['category' => $category->id])}}" method="post">
+    		@csrf
+            @method('PUT')
+        	<div class="row">
+	          	<div class="col-md-12 col-sm-12">
+		            <!-- general form elements -->
+		            <div class="card card-default">
+		            	<div class="card-header">
+
+				            <div class="card-tools">
+				              <button type="button" class="btn btn-tool" data-card-widget="collapse">
+				                <i class="fas fa-minus"></i>
+				              </button>
+				              
+				            </div>
+				        </div>
+				        <!-- /.card-header -->
+		                <div class="card-body">
+							<div class="form-group">
+								<input type="text" name="name" class="form-control" value="{{ $type->name }}" onkeyup="listingslug(this.value)" id="name" placeholder="Category Name" required>
+							</div>
+							<div class="form-group">
+								<input type="text" name="slug" class="form-control" value="{{ $type->slug }}" id="slug" placeholder="Enter Slug" required>
+							</div>
+		                </div>
+		                <!-- /.card-body -->
+		            </div>
+		            <!-- /.card -->
+
+		            <!-- general form elements -->
+		            <div class="card card-default">
+		            	<!-- /.card-body -->
+		                <div class="card-footer">
+		                  	<button type="submit" class="btn btn-primary">Save</button>
+		                  	<a href="{{route('admin.product.type.index')}}" class="btn btn-default float-right">Cancel</a>
+		                </div>
+		                <!-- /.card-footer -->
+		            </div>
+		            <!-- /.card -->
+		        </div>
+        	</div>
+        </form>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+@endsection
+
+@section('admin_page_js')
+	@include('admin.additionalObject.createDocumentScript')
+@endsection
