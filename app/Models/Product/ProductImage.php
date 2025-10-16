7a9d676b9a3e411fe1a34677db82e9aa24b54b94
scottShams <scott123.staff.india@gmail.com>
2025-10-16 22:55:55 +0600

    add accessors with product image

--- app/Models/Product/ProductImage.php
+++ app/Models/Product/ProductImage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Product;
 
+use App\Services\Admin\ImageService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,5 +14,16 @@ class ProductImage extends Model
     public function product()
     {
         return $this->belongsTo(Product::class,'product_id');
+    }
+
+    /**
+     * Get the category's image URL.
+     *
+     * @return string|null
+     */
+    public function getImagePathUrlAttribute(): ?string
+    {
+        $imageService = app(ImageService::class);
+        return $this->image_path ? $imageService->getSingleImageUrl('products/gallery', $this->image_path) : asset('images/default-product.png');
     }
 }
